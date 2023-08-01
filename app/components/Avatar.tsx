dfbd42f06e9a0fc2c5e5ef4a2e4135c3689d778f
keemkeeman <manki9825@gmail.com>
2023-08-01 23:25:12 +0900

    230801-navBar

---
+++ app/components/Avatar.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Image from "next/image";
+
+const Avarta = () => {
+  return <Image className="rounded-full"
+  height="30"
+  width="30"
+  alt="Avatar"
+  src="/images/placeholder.jpg"/>;
+};
+
+export default Avarta;
---
+++ app/components/navbar/MenuItem.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+interface MenuItemProps {
+  onClink: () => void;
+  label: string;
+}
+
+const MenuItem: React.FC<MenuItemProps> = ({ onClink, label }) => {
+  return (
+    <div
+      onClick={onClink}
+      className="px-4 py-3 hover:bg-neutral-100 transition font-semibold"
+    >
+      {label}
+    </div>
+  );
+};
+
+export default MenuItem;
